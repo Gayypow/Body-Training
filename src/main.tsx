dad612ad049e2157ba65ad316e6ec668959356cb
Gayypow <didargayypow2003@gmail.com>
2024-05-27 23:02:05 +0500

    removed unused files

--- src/main.tsx
+++ src/main.tsx
@@ -11,16 +11,3 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
     </BrowserRouter>
   </Provider>
 );
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/sw.js")
-      .then((registration) => {
-        console.log("ServiceWorker registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("ServiceWorker registration failed: ", registrationError);
-      });
-  });
-}
